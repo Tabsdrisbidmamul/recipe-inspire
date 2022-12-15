00c8456930f86dba9a8fb3018efaa5a6802bbaa8
Idris Khan <idris.khan303@gmail.com>
2022-12-15 18:11:40 +0000

    initial action

--- stores/ingredients.store.ts
+++ stores/ingredients.store.ts
@@ -4,7 +4,13 @@ import { makeAutoObservable } from 'mobx';
  * These settings correspond to the ingredients settings page
  */
 export default class IngredientsStore {
+  eggs = false;
+
   constructor() {
     makeAutoObservable(this);
   }
+
+  setEggs = (state: boolean) => {
+    this.eggs = state;
+  };
 }
