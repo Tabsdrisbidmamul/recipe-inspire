fb3c714281af12432c62c79efbebdab5c7a0c395
Idris Khan <idris.khan303@gmail.com>
2022-12-16 20:59:39 +0000

    convert fonts loaded with callback

--- App.tsx
+++ App.tsx
@@ -1,8 +1,8 @@
 import { StyleSheet, Text, View } from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient';
-import { useFonts } from 'expo-font';
+import { useFonts, loadAsync } from 'expo-font';
 import * as SplashScreen from 'expo-splash-screen';
-import React, { useEffect, useCallback } from 'react';
+import React, { useEffect, useCallback, useState } from 'react';
 import colors from './constants/colors';
 import { NavigationContainer } from '@react-navigation/native';
 import Navigation from './components/Navigation/Navigation';
@@ -10,42 +10,65 @@ import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { store, StoreContext } from './stores/store';
 
 export default function App() {
-  const [fontsLoaded] = useFonts({
-    'nunito-regular': require('./assets/fonts/Nunito-Regular.ttf'),
-    'nunito-regular-italic': require('./assets/fonts/Nunito-Italic.ttf'),
-    'nunito-regular-bold': require('./assets/fonts/Nunito-Bold.ttf'),
-    'nunito-light': require('./assets/fonts/Nunito-Light.ttf'),
-    'nunito-light-italic': require('./assets/fonts/Nunito-LightItalic.ttf'),
-    'nunito-medium': require('./assets/fonts/Nunito-Medium.ttf'),
-    'nunito-medium-italic': require('./assets/fonts/Nunito-MediumItalic.ttf'),
-    'nunito-semibold': require('./assets/fonts/Nunito-SemiBold.ttf'),
-    'nunito-semibold-italic': require('./assets/fonts/Nunito-SemiBoldItalic.ttf'),
-  });
+  // const [fontsLoaded] = useFonts({
+  //   'nunito-regular': require('./assets/fonts/Nunito-Regular.ttf'),
+  //   'nunito-regular-italic': require('./assets/fonts/Nunito-Italic.ttf'),
+  //   'nunito-regular-bold': require('./assets/fonts/Nunito-Bold.ttf'),
+  //   'nunito-light': require('./assets/fonts/Nunito-Light.ttf'),
+  //   'nunito-light-italic': require('./assets/fonts/Nunito-LightItalic.ttf'),
+  //   'nunito-medium': require('./assets/fonts/Nunito-Medium.ttf'),
+  //   'nunito-medium-italic': require('./assets/fonts/Nunito-MediumItalic.ttf'),
+  //   'nunito-semibold': require('./assets/fonts/Nunito-SemiBold.ttf'),
+  //   'nunito-semibold-italic': require('./assets/fonts/Nunito-SemiBoldItalic.ttf'),
+  // });
 
-  useEffect(() => {
-    async function prepare() {
-      await SplashScreen.preventAutoHideAsync();
-    }
-    prepare();
+  // useEffect(() => {
+  //   async function prepare() {
+  //     await SplashScreen.preventAutoHideAsync();
+  //   }
+  //   prepare();
+  // }, []);
+
+  // const onLayoutRootView = useCallback(async () => {
+  //   if (fontsLoaded) {
+  //     await SplashScreen.hideAsync();
+  //   }
+  // }, [fontsLoaded]);
+
+  // if (!fontsLoaded) {
+  //   return null;
+  // }
+
+  const [loaded, setLoaded] = useState(false);
+
+  const loadFonts = useCallback(async () => {
+    await loadAsync({
+      'nunito-regular': require('./assets/fonts/Nunito-Regular.ttf'),
+      'nunito-regular-italic': require('./assets/fonts/Nunito-Italic.ttf'),
+      'nunito-regular-bold': require('./assets/fonts/Nunito-Bold.ttf'),
+      'nunito-light': require('./assets/fonts/Nunito-Light.ttf'),
+      'nunito-light-italic': require('./assets/fonts/Nunito-LightItalic.ttf'),
+      'nunito-medium': require('./assets/fonts/Nunito-Medium.ttf'),
+      'nunito-medium-italic': require('./assets/fonts/Nunito-MediumItalic.ttf'),
+      'nunito-semibold': require('./assets/fonts/Nunito-SemiBold.ttf'),
+      'nunito-semibold-italic': require('./assets/fonts/Nunito-SemiBoldItalic.ttf'),
+    });
+    setLoaded(true);
   }, []);
 
-  const onLayoutRootView = useCallback(async () => {
-    if (fontsLoaded) {
-      await SplashScreen.hideAsync();
-    }
-  }, [fontsLoaded]);
-
-  if (!fontsLoaded) {
-    return null;
-  }
+  useEffect(() => {
+    loadFonts();
+  }, [loadFonts]);
 
   return (
     <SafeAreaProvider>
-      <StoreContext.Provider value={store}>
-        <View style={styles.container} onLayout={onLayoutRootView}>
+      {loaded ? (
+        <StoreContext.Provider value={store}>
           <Navigation />
-        </View>
-      </StoreContext.Provider>
+        </StoreContext.Provider>
+      ) : (
+        <></>
+      )}
     </SafeAreaProvider>
   );
 }
--- screens/StackScreen/Ingredients.tsx
+++ screens/StackScreen/Ingredients.tsx
@@ -4,13 +4,37 @@ import React from 'react';
 import { Text, View, StyleSheet } from 'react-native';
 import colors from '../../constants/colors';
 import { globalStyles } from '../../constants/globalStyles';
+import { DetailsHeaderScrollView } from 'react-native-sticky-parallax-header';
+import { StatusBar } from 'expo-status-bar';
+import { Ionicons } from '@expo/vector-icons';
+import { useNavigation } from '@react-navigation/native';
 
 export default observer(function Ingredients() {
+  const navigation = useNavigation();
+
+  function navigateGoBack() {
+    if (navigation.canGoBack()) {
+      navigation.goBack();
+    }
+  }
+
   return (
     <LinearGradient style={styles.root} colors={[colors.gradient.orange, colors.gradient.yellow]}>
-      <View style={styles.root}>
-        <Text style={styles.text}>Ingredients component is working</Text>
-      </View>
+      <DetailsHeaderScrollView
+        leftTopIcon={() => <Ionicons name="arrow-back-sharp" size={24} color="black" />}
+        leftTopIconOnPress={navigateGoBack}
+        containerStyle={styles.headerStyle}
+        contentContainerStyle={[{ backgroundColor: colors.blacks.charcoal }]}
+        backgroundColor={colors.blacks.charcoal}
+        titleStyle={styles.header}
+        title="Ingredients"
+        showsVerticalScrollIndicator={false}
+      >
+        <View style={styles.content}>
+          <Text>A header</Text>
+        </View>
+      </DetailsHeaderScrollView>
+      <StatusBar style="light" />
     </LinearGradient>
   );
 });
@@ -18,10 +42,17 @@ export default observer(function Ingredients() {
 const styles = StyleSheet.create({
   root: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
   },
-  text: {
-    ...globalStyles.baseText,
+  header: {
+    ...globalStyles.baseHeaderText,
+  },
+  headerStyle: {
+    flex: 1,
+    alignSelf: 'stretch',
+  },
+  content: {
+    alignItems: 'center',
+    flex: 1,
+    padding: 24,
   },
 });
